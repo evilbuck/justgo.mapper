66cbfdb33c2f589cc4849ad4c65b8223c850502d
Evil Buck <buckleyrobinson@gmail.com>
2016-10-20 22:10:52 -0700

    sorta working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+const location = require('../lib/locations');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+var secondLegs = [
+  {
+    origin: 'Lafayette Bart Station',
+    mode: 'driving',
+    destination: '405 Howard St San Francisco',
+  },
+  {
+    origin: 'Lafayette Bart Station',
+    mode: 'transit',
+    destination: 'Montgomery St Bart Station',
+  }
+];
+router.get('/from_home', function(req, res) {
+  // location.whichWay()
+  location.compare(secondLegs[0], secondLegs[1])
+    .then((result) => {
+      console.log('result', result);
+      res.send(result)
+    })
+    .fail((error) => {
+      res.status(500).send(error.message);
+    });
 });
 
 module.exports = router;
--- scripts/justgo.js
+++ scripts/justgo.js
@@ -3,8 +3,9 @@ const q = require('q');
 const striptags = require('striptags');
 const moment = require('moment');
 const gMapClient = require('@google/maps').createClient({
-  key: 'AIzaSyC0QP0ZrtipXFkVd5HWS0eH5YiwlwWhy_o',
+  key: process.env.gmapKey,
 });
+const mail = require('../lib/sendgrid');
 
 function getDirections(options) {
   var deferred = q.defer();
@@ -23,12 +24,26 @@ function getDirections(options) {
 var legs = [];
 var differenceBetweenTypes;
 
-getDirections({
-    origin: '5481 Alhambra Valley Rd 94553',
-    destination: 'Lafayette Bart Station',
+var firstLeg = {
+  origin: '5481 Alhambra Valley Rd 94553',
+  destination: 'Lafayette Bart Station',
+  mode: 'driving',
+  alternatives: true,
+};
+var secondLegs = [
+  {
+    origin: 'Lafayette Bart Station',
     mode: 'driving',
-    alternatives: true,
-  })
+    destination: '405 Howard St San Francisco',
+  },
+  {
+    origin: 'Lafayette Bart Station',
+    mode: 'transit',
+    destination: 'Montgomery St Bart Station',
+  }
+];
+
+getDirections(firstLeg)
   .then((result) => {
     var routes = result.json.routes.sort((a, b) => {
       return a.legs[0].duration.value - b.legs[0].duration.value;
@@ -37,18 +52,7 @@ getDirections({
     legs.push(shortest);
   })
   .then(() => {
-    return q.all([
-      getDirections({
-        origin: 'Lafayette Bart Station',
-        mode: 'driving',
-        destination: '405 Howard St San Francisco',
-      }),
-      getDirections({
-        origin: 'Lafayette Bart Station',
-        mode: 'transit',
-        destination: 'Montgomery St Bart Station',
-      })
-    ]);
+    return q.all(secondLegs.map((leg) => getDirections(leg)));
   })
   .spread((driving, transit) => {
     var transitTime = transit.json.routes[0].legs[0].duration.value;
@@ -57,30 +61,34 @@ getDirections({
     var transitArrival = moment(transit.json.routes[0].legs[0].arrival_time.value * 1000);
 
     var stepInstructions = legs[0].legs[0].steps.map((step) => striptags(step.html_instructions));
-    var sms = `${stepInstructions.slice(0, 2).join('\n')}`;
-    sms += `\n...${stepInstructions[stepInstructions.length-1]}`;
+    var sms = `${stepInstructions.slice(0, 2).join('. ')}. `;
+    sms += `...${stepInstructions[stepInstructions.length-1]}`;
     sms += `\ndriving: arrive at ${drivingArrival.calendar()}\ntransit: arrive at ${transitArrival.calendar()}`;
 
     var arrivalTimes = [drivingArrival.toDate(), transitArrival.toDate()];
     sms += `\nthe difference is ${Math.round((Math.max.apply(null, arrivalTimes) - Math.min.apply(null, arrivalTimes))/ 1000 / 60)} minutes.`;
 
-    console.log(sms);
+    var chunk = sms.slice(0, 140);
+    // sms = sms.slice(140);
+    var counter = 0;
+    // TODO: this code stinks
+    // we're only chunking this for sms
+    var emails = [];
+    while(chunk) {
+      if (counter >= 3) break;
+      sms = sms.slice(140);
 
-    //var times = [transitTime, drivingTime];
-    //differenceBetweenTypes = Math.max.apply(null, times) - Math.min.apply(null, times);
+      emails.push(mail('buckleyrobinson@gmail.com', 'traffic', '5612552825@vtext.com', chunk));
+      chunk = sms.slice(0, 140);
+      counter += 1;
+    }
 
-    //var shortestWay = transitTime > drivingTime ? transit.json.routes[0] : driving.json.routes[0];
-
-    //var sms = `${stepInstructions.slice(0, 2).join('\n')}`;
-    //sms += `\n...${stepInstructions[stepInstructions.length-1]}`
-
-    //var transitType = shortestWay.fare ? 'bart' : 'car pool';
-    //sms += `\nThen take the ${transitType.toUpperCase()} (difference is ${Math.round(differenceBetweenTypes/60)} minutes)`;
-    //console.log(sms);
-
+    return q.all(emails);
+  })
+  .then(() => {
     process.exit();
   })
   .fail((error) => {
-    console.error(error);
+    console.error('error', error);
     process.exit(error);
   });
